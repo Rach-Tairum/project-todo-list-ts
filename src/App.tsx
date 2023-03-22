cb114e1ec4e0ce723886a5b6a0bccfe3bf5eed10
Rachel Tairum <rachel.tairum@gmail.com>
2023-03-22 10:01:51 -0300

    Inicia projeto e sua estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import styleApp from './App.module.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <main className={styleApp.main}>Conteudo Elemento X</main>
+     <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import stylesF from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={stylesF.footer}>
+      <p>
+        <span>React + TS TODO </span> @ 2023
+      </p>
+    </footer>
+
+  )
+}
+
+
+export default Footer
